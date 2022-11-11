5276c5967d53c140d5dc1e5c847fa3e34e5ffa17
Andrei Stan <97159805+andreistanac@users.noreply.github.com>
2022-11-11 13:03:32 +0200

    initial commit

---
+++ Debug/Core/optiga/pal/subdir.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/optiga/pal/pal.c \
+../Core/optiga/pal/pal_gpio.c \
+../Core/optiga/pal/pal_i2c.c \
+../Core/optiga/pal/pal_ifx_i2c_config.c \
+../Core/optiga/pal/pal_logger.c \
+../Core/optiga/pal/pal_os_datastore.c \
+../Core/optiga/pal/pal_os_event.c \
+../Core/optiga/pal/pal_os_lock.c \
+../Core/optiga/pal/pal_os_timer.c 
+
+OBJS += \
+./Core/optiga/pal/pal.o \
+./Core/optiga/pal/pal_gpio.o \
+./Core/optiga/pal/pal_i2c.o \
+./Core/optiga/pal/pal_ifx_i2c_config.o \
+./Core/optiga/pal/pal_logger.o \
+./Core/optiga/pal/pal_os_datastore.o \
+./Core/optiga/pal/pal_os_event.o \
+./Core/optiga/pal/pal_os_lock.o \
+./Core/optiga/pal/pal_os_timer.o 
+
+C_DEPS += \
+./Core/optiga/pal/pal.d \
+./Core/optiga/pal/pal_gpio.d \
+./Core/optiga/pal/pal_i2c.d \
+./Core/optiga/pal/pal_ifx_i2c_config.d \
+./Core/optiga/pal/pal_logger.d \
+./Core/optiga/pal/pal_os_datastore.d \
+./Core/optiga/pal/pal_os_event.d \
+./Core/optiga/pal/pal_os_lock.d \
+./Core/optiga/pal/pal_os_timer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/optiga/pal/%.o Core/optiga/pal/%.su: ../Core/optiga/pal/%.c Core/optiga/pal/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F407xx -c -I../Core/Inc -I"C:/Users/worker/Downloads/L2P1SSC/Core/optiga/include" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-optiga-2f-pal
+
+clean-Core-2f-optiga-2f-pal:
+	-$(RM) ./Core/optiga/pal/pal.d ./Core/optiga/pal/pal.o ./Core/optiga/pal/pal.su ./Core/optiga/pal/pal_gpio.d ./Core/optiga/pal/pal_gpio.o ./Core/optiga/pal/pal_gpio.su ./Core/optiga/pal/pal_i2c.d ./Core/optiga/pal/pal_i2c.o ./Core/optiga/pal/pal_i2c.su ./Core/optiga/pal/pal_ifx_i2c_config.d ./Core/optiga/pal/pal_ifx_i2c_config.o ./Core/optiga/pal/pal_ifx_i2c_config.su ./Core/optiga/pal/pal_logger.d ./Core/optiga/pal/pal_logger.o ./Core/optiga/pal/pal_logger.su ./Core/optiga/pal/pal_os_datastore.d ./Core/optiga/pal/pal_os_datastore.o ./Core/optiga/pal/pal_os_datastore.su ./Core/optiga/pal/pal_os_event.d ./Core/optiga/pal/pal_os_event.o ./Core/optiga/pal/pal_os_event.su ./Core/optiga/pal/pal_os_lock.d ./Core/optiga/pal/pal_os_lock.o ./Core/optiga/pal/pal_os_lock.su ./Core/optiga/pal/pal_os_timer.d ./Core/optiga/pal/pal_os_timer.o ./Core/optiga/pal/pal_os_timer.su
+
+.PHONY: clean-Core-2f-optiga-2f-pal
+
